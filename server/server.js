fa4028162262348ac959d20080d8aff14d4b3766
Archie <archiealvarado20@gmail.com>
2026-04-28 14:24:37 +0800

    it's good refresh3

--- server/server.js
+++ server/server.js
@@ -2,25 +2,28 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
-app.use(cors());
+const cors = require("cors");
 const passport = require("passport");
+
 require("./config/passport");
 
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const ticketRoutes = require("./routes/ticketRoutes");
 const eventRoutes = require("./routes/eventRoutes.js");
 const ticketTypeRoutes = require("./routes/ticketTypeRoutes");
-
 const testRoutes = require("./routes/testRoutes");
 
 const app = express();
 
+// DB
 connectDB()
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("DB Error:", err));
 
+// CORS (PRODUCTION SAFE)
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://orboevents.vercel.app"],
@@ -29,22 +32,22 @@ app.use(
 );
 
 app.use(express.json());
-
-// now safe to use app
 app.use(passport.initialize());
-app.use("/api/ticket-types", ticketTypeRoutes);
 
+// ROUTES
+app.use("/api/ticket-types", ticketTypeRoutes);
 app.use("/api", testRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", ticketRoutes);
-
 app.use("/api", eventRoutes);
 
+// HEALTH CHECK
 app.get("/", (req, res) => {
   res.send("Smart Ticketing API Running");
 });
 
+// START SERVER
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
