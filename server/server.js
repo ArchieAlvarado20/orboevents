0cca679fd7eafeb8ebe28b433d6eac2cd7f50804
Archie <archiealvarado20@gmail.com>
2026-04-29 13:09:12 +0800

    callback now

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ connectDB()
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://orboevents.vercel.app"],
+    origin: "https://orboevents.vercel.app",
     credentials: true,
   }),
 );
