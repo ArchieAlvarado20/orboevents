b290c10bf69b604a0099f71c959b4d2046e92959
Archie <archiealvarado20@gmail.com>
2026-05-07 23:45:37 +0800

    Update admin roles module1

--- server/server.js
+++ server/server.js
@@ -27,11 +27,7 @@ connectDB()
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://orboevents.vercel.app",
-      "https://orboevents-git-dev-mode-archiealvarado20-2377s-projects.vercel.app",
-    ],
+    origin: ["http://localhost:5173", "https://orboevents.vercel.app"],
     credentials: true,
   }),
 );
